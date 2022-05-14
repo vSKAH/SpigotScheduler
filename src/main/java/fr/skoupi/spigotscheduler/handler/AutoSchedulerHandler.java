4f7f9cae7fcf2f831b7f84f9c2ca11d63102ce89
vSKAH <vskahhh@gmail.com>
2022-05-14 15:31:43 +0200

    Add: %players% variable

--- src/main/java/fr/skoupi/spigotscheduler/handler/AutoSchedulerHandler.java
+++ src/main/java/fr/skoupi/spigotscheduler/handler/AutoSchedulerHandler.java
@@ -10,6 +10,7 @@ import fr.skoupi.spigotscheduler.SpigotScheduler;
 import fr.skoupi.spigotscheduler.manager.AutoSchedulerManager;
 import fr.skoupi.spigotscheduler.utils.ExecutorServiceUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -30,7 +31,21 @@ public class AutoSchedulerHandler {
         AutoSchedulerManager.getInstance().getAutoScheduler(schedulerName).buildAutoSchedulerModel(((calendar, autoSchedulerModel) -> {
             long firstStart = calendar.getTime().getTime() - System.currentTimeMillis();
             long repeatPeriod = TimeUnit.DAYS.toMillis(7);
-            ExecutorServiceUtil.execute(() -> Bukkit.getScheduler().runTask(plugin, () -> autoSchedulerModel.getSchedulerCommand().forEach(cmd -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd))), firstStart, repeatPeriod, TimeUnit.MILLISECONDS);
+            ExecutorServiceUtil.execute(() -> {
+                Bukkit.getScheduler().runTask(plugin, () -> {
+
+                    for (String cmd : autoSchedulerModel.getSchedulerCommand()) {
+
+                        if(cmd.contains("%players%")) {
+                            for (Player player : Bukkit.getOnlinePlayers()) {
+                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replace("%players%", player.getName()));
+                            }
+                        }
+                        else Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+                    }
+
+                });
+            }, firstStart, repeatPeriod, TimeUnit.MILLISECONDS);
         }));
     }
 
